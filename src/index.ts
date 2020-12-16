a745880560930e1f3f310d50df65d63b1fb03bfe
oasis10702 <oasis10702@gmail.com>
2020-12-16 18:19:49 +0900

    format data

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,6 @@ import { CsvFileReader } from './CsvFileReader';
 const reader = new CsvFileReader('football.csv');
 reader.read();
 
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D'
-}
-
 let manUnitedWins = 0;
 
 for (let match of reader.data) {
