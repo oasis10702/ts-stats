e8e96449bc7774a93f3e89999f5eda3e721310b9
oasis10702 <oasis10702@gmail.com>
2020-12-21 20:29:55 +0900

    add html report

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { HtmlReport } from './reportTargets/HtmlReport';
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
 
@@ -13,6 +14,6 @@ const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport());
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
 
 summary.buildAndPrintReport(matchReader.matches);
